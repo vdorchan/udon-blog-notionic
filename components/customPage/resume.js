a6fc4004cf5a4ae60494f4f4b041eaee0fce5cf5
vdorchan <vdorchan@gmail.com>
2023-12-23 00:50:35 +0800

    feat: resume screet

--- components/customPage/resume.js
+++ components/customPage/resume.js
@@ -2,12 +2,32 @@ import { useEffect, useState, useCallback } from 'react'
 import BLOG from '@/blog.config'
 import NotionRenderer from '@/components/Post/NotionRenderer'
 
+const screetText = (blockMap, blockIds) => {
+  if (location.hostname === 'localhost') return
+  try {
+    blockIds.forEach((blockId) => {
+      const key = Object.keys(blockMap.block).find(
+        (key) => key.replaceAll('-', '') === blockId
+      )
+      const block = blockMap.block[key]
+      block.value.properties.title[0] = ['***']
+    })
+  } catch (error) {}
+}
+
 export default function Resume({ blockMap, authorName }) {
   const checkPassword = useCallback(
     (value) => value === authorName,
     [authorName]
   )
   const [hide, setHide] = useState(true)
+
+  useEffect(() => {
+    screetText(blockMap, [
+      '9b4647f8a10f429b8bbba10de1ef2924',
+      'e0fe2289bdf245319a2c3589e986cd9a'
+    ])
+  }, [blockMap])
 
   useEffect(() => {
     document.documentElement.classList.add('print')
@@ -20,21 +40,30 @@ export default function Resume({ blockMap, authorName }) {
     } catch (error) {}
   }, [checkPassword])
 
+  const onSubmit = (value) => {
+    if (checkPassword(value)) {
+      sessionStorage.setItem(
+        'resume_password',
+        window.btoa(unescape(encodeURIComponent(value)))
+      )
+      setHide(false)
+    }
+  }
+
   return hide ? (
     <div className='h-screen flex justify-center items-center'>
       <input
         type='text'
         placeholder='输入我的姓名拼音查看简历'
-        className='mx-auto bg-white dark:bg-gray-600 shadow-md rounded-lg outline-none focus:shadow p-3'
+        className='mx-auto bg-white dark:bg-gray-600 shadow-md rounded-lg outline-none focus:shadow p-3 w-80'
         onKeyPress={(e) => {
           const { value } = e.target
-          if (e.charCode === 13 && checkPassword(value)) {
-            sessionStorage.setItem(
-              'resume_password',
-              window.btoa(unescape(encodeURIComponent(value)))
-            )
-            setHide(false)
+          if (e.charCode === 13) {
+            onSubmit()
           }
+        }}
+        onChange={(e) => {
+          onSubmit(e.target.value)
         }}
       />
     </div>
